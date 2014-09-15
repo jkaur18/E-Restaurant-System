92013705a23e74cbd4ca61679fd391722457ceb4
jkaur18 <jkaur18@studentmail.nait.ca>
2014-09-15 16:55:24 -0600

    Created Component Library Code
    Created Entity special event , Created context class and created
    component library interface method

---
+++ eResturantSystem/BLL/eResturantController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eResturantSystem.Entities;
+using eResturantSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace eResturantSystem.BLL
+{
+    [DataObject]
+    public class eResturantController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            //interfacing with our Context class which is inherites DBContext which ties into entity framework
+            using (eResturantContext context = new eResturantContext())
+            {
+                return context.SpecialEvents.ToList();
+
+            }
+        }
+    }
+}
---
+++ eResturantSystem/DAL/eResturantContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eResturantSystem.Entities;
+using System.Data.Entity;
+#endregion
+
+namespace eResturantSystem.DAL
+{
+    // hookup to entity framework via the DbContext base class
+    internal class eResturantContext: DbContext
+    {
+        //constructor pass to the base class the name value for the 
+        //connection string to the database found in the WebConnectionStrings.Config
+        public eResturantContext() : base("name=EatIn") { }
+        //one DB set is created for each entity to be reference by your application
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        //public DbSet<Reservation> SpecialEvents { get; set; }
+        //public DbSet<Table> SpecialEvents { get; set; }
+    }
+}
